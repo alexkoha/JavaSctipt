8972c934b68266141b7b26dff9e3d6f76abb8788
Alex <alexkoha@gmail.com>
2019-01-19 20:02:42 +0200

    Add files via upload

--- TextTyping/script.js
+++ TextTyping/script.js
@@ -3,26 +3,60 @@ const textArea = document.querySelector("#text-area");
 const time = document.querySelector(".timer");
 const reset = document.querySelector("#reset");
 
-console.log(originalText);
+function timeHand(num) {
+  if(num<10){
+    return "0"+num;
+  };
+  return num;
+}
+
+var timer=[0,0,0,0];
+var timerPointer;
+function timerUp(){
+  let timeShow ;
+  timer[0] ++;
+  timer[1] = Math.floor(timer[0]-(Math.floor(timer[0]/100)*100));
+  timer[2] = Math.floor(timer[0]/100)%60;
+  timer[3] = Math.floor(Math.floor(timer[0]/100)/60);
+
+  timeShow = timeHand(timer[3]) + ":" + timeHand(timer[2]) + ":" + timeHand(timer[1]);
+
+  time.innerHTML = timeShow;
+}
+
 
 function gotNewChar(){
   console.log(textArea.value);
-  if(textArea.value.lenght===1){
-    setInterval(clockUp,10);
+  if(textArea.value===originalText.slice(0,textArea.value.length)){
+    textArea.style.borderColor="green";
+    if(textArea.value===originalText){
+      console.log("match");
+      clearInterval(timerPointer);
+    }
+  } else {
+    textArea.style.borderColor="red";
+    console.log("Not match");
   }
 };
 
-var timer=0;
-function clockUp(){
-  timer ++;
-  time.innerHTML = timer;
+function gotKeyPress(){
+  if(textArea.value==="" & timerPointer==null){
+    timerPointer = setInterval(timerUp,10);
+  }
 }
 
 function resetClicked() {
   console.log("reset clicked");
+  clearInterval(timerPointer);
+  timerPointer = null;
+  timer = [0,0,0,0];
+  time.innerHTML = "00:00:00";
+  textArea.value = "";
+  textArea.style = "";
 };
 
 console.log(originalText);
 
 textArea.addEventListener("keyup",gotNewChar,false);
+textArea.addEventListener("keypress",gotKeyPress,false);
 reset.addEventListener("click",resetClicked,false);
